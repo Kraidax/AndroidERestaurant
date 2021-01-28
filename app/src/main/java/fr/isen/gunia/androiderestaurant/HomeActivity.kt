b2c5b749973eb70df2c943483ef39ad508c0deaa
Kraidax <66353063+Kraidax@users.noreply.github.com>
2021-01-28 16:42:30 +0100

    New ADD
    Toast + new page : ListActivity

--- app/src/main/java/fr/isen/gunia/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/gunia/androiderestaurant/HomeActivity.kt
@@ -1,11 +1,36 @@
 package fr.isen.gunia.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import fr.isen.gunia.androiderestaurant.databinding.ActivityHomeBinding
 
-class MainActivity : AppCompatActivity() {
+private lateinit var binding: ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.BEntrees.setOnClickListener{
+            val toast = Toast.makeText(applicationContext,"Vous êtes sur les entrées", Toast.LENGTH_SHORT)
+            toast.show()
+            val intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
+        }
+        binding.BPlats.setOnClickListener{
+            val toast = Toast.makeText(applicationContext,"Vous êtes sur les plats", Toast.LENGTH_SHORT)
+            toast.show()
+            val intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
+        }
+        binding.BDesserts.setOnClickListener{
+            val toast = Toast.makeText(applicationContext,"Vous êtes sur les desserts", Toast.LENGTH_SHORT)
+            toast.show()
+            val intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
